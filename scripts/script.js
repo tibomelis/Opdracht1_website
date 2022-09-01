376b3a0fcbbdec3ded107f43e76a254fdd5e1424
Tibo <tibomelis71004@gmail.com>
2022-09-01 18:24:06 +0200

    whoops. vergeten te commiten

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+// for (let i = 0; i < 6; i++) {
+//     const img = document.createElement('img');
+//     const imagesdiv = document.querySelector('div.images');
+
+//     img.src = './../images/image_template.png';
+
+//     img.style.width = '45%';
+//     img.style.height = 'auto';
+//     img.style.padding = '5px';
+//     img.style.zIndex = 3;
+//     img.addEventListener('mouseenter', makehoverimg);
+//     img.addEventListener('mouseleave', delhoverimg);
+//     imagesdiv.appendChild(img);
+// }
